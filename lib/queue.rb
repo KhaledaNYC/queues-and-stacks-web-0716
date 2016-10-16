0cb60adbd8cdc89fd0e16ca14a1d146986fffd84
KhaledaNYC <khaleda.khan87@gmail.com>
2016-10-16 12:39:10 -0400

    enqueue push data el first stack, dequeue pop data from first stack and push into second stack til first stack is empty then return last element of the second stack which turns out to be the first el of the first stack FIFO

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,27 @@
 require_relative 'stack'
 
 class Queue
+  def initialize
+    @in = Stack.new
+    @out = Stack.new
+  end
+#pushes data into the first stack
+  def enqueue(data)
+    @in.push(data)
+  end
 
+  def dequeue
+    # checks size of the out stack
+    if @out.size == 0
+      #as long as the in stack is greater than zero
+      while @in.size > 0
+      #remove the last element from in stack
+        last_in = @in.pop
+      #push last element of in stack into outstack
+        @out.push(last_in)
+      end
+    end
+    #as soon as in stack is less than zero return last element of out stack which turns out to be the first el of the in stack (initially)
+    @out.pop
+  end
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,19 @@
 class Stack
+  def initialize
+    @stack = []
+  end
+
+  def push(data)
+    @stack.push(data)
+    self
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def size
+    @stack.length
+  end
 
 end
